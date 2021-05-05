8867e3c63539e41237f4e293247921ed7fa376f4
marqpad <11246294+marqpad@users.noreply.github.com>
2021-05-05 19:49:03 +0200

    Added get info from the codes for comparisong

--- src/Mastermino.cpp
+++ src/Mastermino.cpp
@@ -22,16 +22,33 @@ void Mastermino::newEntry(uint32_t entryColor)
 	if (!(guessCount > COLUMNS))
 	{
 		holder[turnCount].dot[guessCount].color = entryColor;
+		holder[turnCount].dot[guessCount].index = guessCount;
+
 		guessCount++;
 	}
 }
 
 void Mastermino::finishGuess()
 {
-	masterCode.dot[0].color = COLOR_YELLOW;
-	masterCode.dot[1].color = COLOR_BLUE;
-	masterCode.dot[2].color = COLOR_PINK;
-	masterCode.dot[3].color = COLOR_YELLOW;
+	masterCode.dot[0].color = COLOR_PINK;
+	masterCode.dot[0].index = 0;
+	masterCode.dot[1].color = COLOR_RED;
+	masterCode.dot[1].index = 1;
+	masterCode.dot[2].color = COLOR_BLUE;
+	masterCode.dot[2].index = 2;
+	masterCode.dot[3].color = COLOR_ORANGE;
+	masterCode.dot[3].index = 3;
+
+	//Tesst
+	holder[turnCount].dot[0].color = COLOR_BLUE;
+	holder[turnCount].dot[0].index = 0;
+	holder[turnCount].dot[1].color = COLOR_ORANGE;
+	holder[turnCount].dot[1].index = 1;
+	holder[turnCount].dot[2].color = COLOR_PINK;
+	holder[turnCount].dot[2].index = 2;
+	holder[turnCount].dot[3].color = COLOR_PINK;
+	holder[turnCount].dot[3].index = 3;
+
 	guessCount = COLUMNS;
 
 	//Only reset values when it is not the last line
@@ -42,57 +59,75 @@ void Mastermino::finishGuess()
 
 		//Set markers on each Holder
 		//Check mastercode
-		int masterCode_differentColors = 1; // 1 cause allways there is atleast 1 color in the code
-		int masterCode_repeatedColors = 0;
+
+		int masterCode_colors_count[COLORS_SIZE]; //Save the color with the count of repeated colors
+		int guessCode_colors_count[COLORS_SIZE];
 		//Block the over check of the values
 		bool masterCode_checked[COLUMNS];
-		for (int i = 0; i < COLUMNS; i++)
+		for (int i = 0; i < COLORS_SIZE; i++)
 		{
-			masterCode_checked[i] = false;
+			masterCode_colors_count[i] = 0;
+			guessCode_colors_count[i] = 0;
 		}
 
-		//Compare each Dot with each other to check the repeated colors
+		//Save each color count
 		for (int i = 0; i < COLUMNS; i++)
 		{
-			if (!masterCode_checked[i])
+			masterCode_colors_count[masterCode.dot[i].color]++;
+			guessCode_colors_count[holder[turnCount].dot[i].color]++;
+		}
+
+		//Print colors count info
+		Serial.println("MasterCode info: ");
+		for (int c = 0; c < COLORS_SIZE; c++)
+		{
+			if (masterCode_colors_count[c])
 			{
-
-				for (int c = 0; c < COLUMNS; c++)
-				{
-					if (!masterCode_checked[c])
-					{
-
-						if (masterCode.dot[i].color != masterCode.dot[c].color)
-						{
-
-							masterCode_differentColors++;
-							
-						}
-						else if(masterCode.dot[i].color == masterCode.dot[c].color) 
-						{
-							Serial.print("Index: ");
-							Serial.print(i);
-							Serial.println(" ");
-							Serial.println("color reepated: ");
-							Serial.println(masterCode.dot[i].color);
-							masterCode_repeatedColors++;
-						}
-						
-					}
-					masterCode_checked[i] = true;
-				}
+				Serial.print("Color n");
+				Serial.print(c);
+				Serial.print(" is ");
+				Serial.println(masterCode_colors_count[c]);
+				Serial.println(" times");
 			}
 		}
+		Serial.println();
+		Serial.println("GUESS info");
+		for (int c = 0; c < COLORS_SIZE; c++)
+		{
+			if (guessCode_colors_count[c])
+			{
+				Serial.print("Color n");
+				Serial.print(c);
+				Serial.print(" is ");
+				Serial.println(guessCode_colors_count[c]);
+				Serial.println(" times");
+			}
+		}
+		Serial.println();
 
-		Serial.print("Different colors in the code: ");
-		Serial.println(masterCode_differentColors);
-		Serial.print("Repeated colors in the code: ");
-		Serial.println(masterCode_repeatedColors);
+		//Now compare the guess with the masterCode to see how correct is the guess
 
-		// Clear again the checked colors
+		bool colorCheck[COLUMNS];
 		for (int i = 0; i < COLUMNS; i++)
 		{
-			masterCode_checked[i] = false;
+			colorCheck[i] = false;
+		}
+		// comapre guess and mastercode to see comparisons
+		for (int i = 0; i < COLUMNS; i++)
+		{
+			Dots masterColor = masterCode.dot[i];
+			if (guessCode_colors_count[masterColor.color] && !colorCheck[i])
+			{
+				if (masterColor.color == holder[turnCount].dot[i].color)
+				{
+					Serial.println("COLOR and POS ok");
+				}
+				else
+				{
+					
+				}
+				colorCheck[i] = true;
+			}
 		}
 
 		//Set the last holder guessed, and increase turn
